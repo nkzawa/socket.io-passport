0ce60c76736fd02b26e26e704a87a3680d63a3f4
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2013-08-28 03:07:52 +0900

    enable to use a non-default Passport instance

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var passport = require('passport');
 
+/**
+ * Socket.IO-passport constructor.
+ *
+ * @param {Passport} passport instance to be based
+ * @api public
+ */
 
-exports.initialize = function() {
-  var fn = passport.initialize();
+function Passport(passport) {
+  this.passport = passport || require('passport');
+}
+
+/**
+ * Passport's primary initialization middleware.
+ *
+ * Options:
+ *   - `userProperty`  Property to set on `req` upon login, defaults to `user`
+ *
+ * @param {Object} options
+ * @return {Function} middleware
+ * @api public
+ */
+
+Passport.prototype.initialize = function(options) {
+  var fn = this.passport.initialize(options);
 
   return function initialize(socket, next) {
     var req = socket.request
@@ -12,8 +32,16 @@ exports.initialize = function() {
   };
 };
 
-exports.session = function() {
-  var fn = passport.session();
+/**
+ * Middleware that will restore login state from a session.
+ *
+ * @param {Object} options
+ * @return {Function} middleware
+ * @api public
+ */
+
+Passport.prototype.session = function(options) {
+  var fn = this.passport.session(options);
 
   return function session(socket, next) {
     var req = socket.request
@@ -22,4 +50,19 @@ exports.session = function() {
     fn(req, res, next);
   };
 };
+
+
+/**
+ * Export default singleton.
+ *
+ * @api public
+ */
+
+exports = module.exports = new Passport();
+
+/**
+ * Expose constructors.
+ */
+
+exports.Passport = Passport;
 
--- test/index.js
+++ test/index.js
@@ -1,17 +1,23 @@
-var passport = require('../')
-  , expect = require('chai').expect;
+var Passport = require('passport').Passport
+  , passport = require('../')
+  , expect = require('chai').expect
+  , IOPassport = passport.Passport;
 
 
 describe('socket.io-passport', function() {
+  beforeEach(function() {
+    this.passport = new IOPassport(new Passport());
+  });
+
   it('should create initialization middleware', function() {
-    var initialize = passport.initialize();
+    var initialize = this.passport.initialize();
     expect(initialize).to.be.a('function');
     expect(initialize.length).to.equal(2);
   });
 
   it('should create session restoration middleware', function() {
-    var session = passport.session();
+    var session = this.passport.session();
     expect(session).to.be.a('function');
     expect(session.length).to.equal(2);
-  })
+  });
 });
